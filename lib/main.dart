bde6c447089e6b588a994a6654e95411d863bdc5
Mortaza-Karimi <salambarf@gmail.com>
2022-09-08 15:52:53 +0430

    learn google signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,11 +8,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -18,14 +20,45 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<void> signinWithGoogelAccount(BuildContext context) async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+      final AuthCredential authCredential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+      UserCredential result = await auth.signInWithCredential(authCredential);
+      User? user = result.user;
+      if (user != null) {
+        debugPrint("$user");
+      }
+    }
+
+    return;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Signin With Google")),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => signinWithGoogelAccount(context),
+          child: const Text("Signin With Googel Account"),
+        ),
+      ),
+    );
   }
 }
